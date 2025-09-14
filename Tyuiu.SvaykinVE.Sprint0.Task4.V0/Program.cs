178753520117afafc3edc06ca7fcb8587412b531
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-09-15 02:00:16 +0500

    Task4

---
+++ Tyuiu.SvaykinVE.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.SvaykinVE.Sprint0.Task4.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Addition(1, 5));
+        Console.WriteLine(DataService.Subtraction(1, 5));
+        Console.WriteLine(DataService.Multiplication(1, 5));
+        Console.WriteLine(DataService.Division(1, 5));
+        Console.ReadKey();
+    }
+}
