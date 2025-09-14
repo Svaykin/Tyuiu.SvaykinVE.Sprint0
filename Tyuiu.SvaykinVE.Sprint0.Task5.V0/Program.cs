fe1f9c9176098f99f5cf5b79092f6118ce18fbac
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-09-15 02:19:03 +0500

    Task5

---
+++ Tyuiu.SvaykinVE.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.SvaykinVE.Sprint0.Task5.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Division(10, 5));
+        Console.WriteLine(DataService.Division(10, 0));
+    }
+}
