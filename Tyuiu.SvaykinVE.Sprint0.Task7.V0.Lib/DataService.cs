71178461d4caf16f9bb34ce27acd4cf1f6f74708
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-09-16 01:09:13 +0500

    Task7

---
+++ Tyuiu.SvaykinVE.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.SvaykinVE.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numeOne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = numeOne[i] + numTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
